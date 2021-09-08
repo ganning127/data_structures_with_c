4b9dba8091f52c245678369f2c2f42f60d52953c
Ganning Xu <ganningxu@gmail.com>
2021-09-07 23:15:47 -0400

    add sorting hw folder

---
+++ recursive_sorting_hw/main.c
@@ -0,0 +1,207 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define ARR_LENGTH 100
+#define MAX_NUM 100
+
+void selection_sort(int arr[], int n, size_t i);
+void insertion_sort(int arr[], size_t i);
+void insert(int arr[], size_t i);
+size_t max_index(int *arr, size_t i);
+int random_int(int min, int max);
+void fillArray(int arr[], size_t length);
+void showArr(int arr[], size_t length);
+int getMedian(int arr[], size_t length);
+void showMode(int arr[], size_t length);
+
+double getAverage(int arr[], size_t length);
+
+int main()
+{
+    int arr[ARR_LENGTH];
+    srand(42);
+
+    fillArray(arr, ARR_LENGTH);
+
+    printf("Before any sorting\n");
+    showArr(arr, ARR_LENGTH);
+
+    printf("\n");
+
+    selection_sort(arr, ARR_LENGTH, ARR_LENGTH - 1); // recursive selection sort
+    printf("After selection sort in ascending order\n");
+    showArr(arr, ARR_LENGTH);
+
+    // analysis
+    int median = getMedian(arr, ARR_LENGTH);
+    double avg = getAverage(arr, ARR_LENGTH);
+
+    printf("\n");
+
+    insertion_sort(arr, ARR_LENGTH - 1);
+    printf("After selection sort in descending order\n");
+    showArr(arr, ARR_LENGTH);
+
+    printf("\n");
+    printf("Mean: %.1lf\n", avg);
+    printf("Median: %.1f\n", (double)median);
+    showMode(arr, ARR_LENGTH);
+    return 0;
+}
+
+void showMode(int arr[], size_t length)
+{
+    // create array with 100 elements (1-100)
+    // loop through all elements in the array
+    // for each element, add them to its index value
+    // look through array to find the max value
+
+    int counts[MAX_NUM + 1];
+    // populate array with zeros
+    for (size_t j = 0; j < MAX_NUM + 1; ++j)
+    {
+        counts[j] = 0;
+    }
+
+    for (size_t i = 0; i < length; ++i)
+    {
+        ++counts[arr[i]]; // increment the count of the index (value of element is index)
+    }
+
+    // find the max element in the counts array
+    int max_index = 0;
+    for (size_t z = 0; z < MAX_NUM + 1; ++z)
+    {
+        if (counts[z] > counts[max_index])
+            max_index = z;
+    }
+
+    // loop through all elements in the array and print elements with values >= max_index
+    printf("Mode: ");
+    for (size_t f = 0; f < MAX_NUM + 1; ++f)
+    {
+        if (counts[f] >= counts[max_index])
+            printf("%zu ", f);
+    }
+    printf("\n");
+}
+
+double getAverage(int arr[], size_t length)
+{
+    int sum = 0;
+
+    for (size_t i = 0; i < length; ++i)
+    {
+        sum += arr[i];
+    }
+    int avg = (double)sum / ARR_LENGTH;
+    return avg;
+}
+
+int getMedian(int arr[], size_t length)
+{
+    float median = 0;
+
+    // if number of elements are even
+    if (length % 2 == 0)
+        median = (arr[(length - 1) / 2] + arr[length / 2]) / 2.0;
+    // if number of elements are odd
+    else
+        median = arr[length / 2];
+
+    return median;
+}
+
+void fillArray(int arr[], size_t length)
+{
+    for (size_t i = 0; i < length; ++i)
+    {
+        arr[i] = random_int(0, MAX_NUM);
+    }
+}
+
+void showArr(int arr[], size_t length)
+{
+    for (size_t i = 0; i < length; ++i)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int random_int(int min, int max)
+{
+    return min + rand() % (max + 1 - min);
+}
+
+size_t max_index(int *arr, size_t i)
+{
+    // get max index of the elements in subarray arr[0..i]
+    if (i == 0)
+        return 0;
+    size_t max_i = max_index(arr, i - 1);
+    if (arr[i] > arr[max_i])
+    {
+        return i;
+    }
+    else
+    {
+        return max_i;
+    }
+}
+
+void selection_sort(int arr[], int n, size_t i)
+{
+    if (i == 0) // base case
+        return;
+
+    size_t max_i = max_index(arr, i);
+
+    // swap current element and element at max index
+    int temp = arr[i];
+    arr[i] = arr[max_i];
+    arr[max_i] = temp;
+    selection_sort(arr, n, i - 1);
+}
+
+void insert(int arr[], size_t i)
+{
+    if (i == 0)
+        return;
+
+    if (arr[i] > arr[i - 1])
+    {
+        int temp = arr[i];
+        arr[i] = arr[i - 1];
+        arr[i - 1] = temp;
+    }
+
+    insert(arr, i - 1);
+}
+
+void insertion_sort(int arr[], size_t i)
+{
+    if (i == 0)
+        return;
+
+    insertion_sort(arr, i - 1);
+
+    insert(arr, i);
+}
+
+/*
+    function insert()
+        base case: 
+            at index 0, just use return; (no value)
+        otherwise: 
+            compare to prev element, and swap if necessary.
+            recursive function call on array and index-1
+
+    function insertion_sort()
+        **start at end of the array (pass in size)
+        base_case:
+            at the beginning, juse use return; (no value)
+        otherwise:
+            recursive function call on array and index-1
+            insert current index into sorted array using insert().
+            decrement static index
+*/

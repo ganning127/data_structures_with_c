5970f8f042a142b688b3898886e8ff3c006bf599
Ganning Xu <ganningxu@gmail.com>
2021-09-21 18:54:57 -0400

    finish lldelete

--- shuffling_attendance/main.c
+++ shuffling_attendance/main.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define NUM_SUITS 4
+#define NUM_FACES 13
+
+typedef struct card
+{
+    char *face;
+    char *strings;
+    unsigned int number;
+
+    struct card *friend; // student.friend is a pointer
+} Card;
 
 void fillDeck(Card *deck, char *faces[], char *suits[]);
 void shuffle(Card *deck);
@@ -7,6 +18,6 @@ void deal(Card *deck);
 
 int main(void)
 {
-
+    srand(42); // seed random number generator
     return 0;
 }

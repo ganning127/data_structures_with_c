c07532451bbc49404ca429d749d21f6925e06458
Ganning Xu <ganningxu@gmail.com>
2021-10-22 13:11:33 -0400

    working version of al_insert

--- hw_bst_arraylist/main.c
+++ hw_bst_arraylist/main.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define INITIAL_CAPACITY 1;
+
+typedef struct node
+{
+    int key;
+} Node;
+
 typedef struct arraylist
 {
     unsigned int capacity; // value
-    int *array;            // pointer to the next element
+    Node **array;          // array of NodePtr pointers
 } ArrayList;
 // it should be an array of struct pointers
 // each struct should contain key
@@ -19,41 +25,62 @@ size_t left_child(size_t index);
 size_t right_child(size_t index);
 size_t parent_index(size_t index);
 void print_level_order(ArrayList *array_list);
-
+void print_pointers(ArrayList *list);
 int main(void)
 {
     ArrayList *nu = al_create();
+
     al_insert(nu, 4);
     al_insert(nu, 3);
     al_insert(nu, 5);
     al_insert(nu, 2);
     al_insert(nu, 6);
+    al_insert(nu, 65);
+    al_insert(nu, 1);
+    // print_pointers(nu);
+    print_level_order(nu);
 
     return 0;
 }
 
+void print_pointers(ArrayList *list)
+{
+    for (size_t i = 0; i < list->capacity; ++i)
+        printf("%p ", list->array[i]);
+
+    puts("");
+}
 void al_insert(ArrayList *list, int key)
 {
-    size_t i = 0;
-    // for (i = list->capacity++; i > 0 && list->array[i - 1] > key; --i)
-    // {
-    //     // similar to insertion sort
-    //     // list->array[i - 1] > key, then list->array[i - 1] needs to be shifted towards the end by 1
-    //     list->array[i] = list->array[i - 1];
-    // }
+    // printf("key called: %d\n", key);
+    size_t index = 0;
+    size_t items = 0;
 
-    while (list->array[i] != '\0')
+    // count how many items are in list and resize if necesary
+
+    while (list->array[index] != NULL)
     {
-        if (key < list->array[i])
+        printf("%p\n", list->array[index]);
+        if (key < list->array[index]->key)
         {
-            // insert to the left of current node
-            i = right_child(0);
+            // look at left child
+            index = left_child(index);
         }
-        else
+        else if (key >= list->array[index]->key)
         {
-            // insert to the right of the current node
+            // look at right child
+            index = right_child(index);
         }
+        if (index >= list->capacity)
+            resize(list, list->capacity);
     }
+
+    // resize(list, list->capacity);
+    // printf("insert index: %zu\n", index);
+    // insert here
+    Node *nu = malloc(sizeof(Node));
+    nu->key = key;
+    list->array[index] = nu;
 }
 
 ArrayList *al_create()
@@ -61,12 +88,7 @@ ArrayList *al_create()
     ArrayList *nu = malloc(sizeof(ArrayList)); // allocate memory for the struct itself
     nu->capacity = INITIAL_CAPACITY;
 
-    nu->array = malloc(nu->capacity * sizeof(int)); // setting elements in the array to 0
-
-    for (size_t i = 0; i < nu->capacity; ++i)
-    {
-        nu->array[i] = '\0';
-    }
+    nu->array = calloc(nu->capacity, sizeof(Node *)); // setting elements in the array to 0
 
     return nu;
 }
@@ -84,14 +106,19 @@ size_t parent_index(size_t index)
 void print_level_order(ArrayList *array_list)
 {
     for (size_t i = 0; i < array_list->capacity; ++i)
-        printf("%d ", array_list->array[i]);
+    {
+        if (array_list->array[i] == NULL)
+            printf("NULL ");
+        else
+            printf("%d ", (array_list->array[i])->key);
+    }
     printf("\n");
 }
 
 void resize(ArrayList *list, unsigned int old_capacity)
 {
     list->capacity = old_capacity * 2 + 1;
-    list->array = realloc(list->array, list->capacity * sizeof(int)); // `capacity` is like the number of elements. we need to multiply by the sizeof(int) to get the true number of bytes that need to be allocateted
+    list->array = realloc(list->array, list->capacity * sizeof(Node *)); // `capacity` is like the number of elements. we need to multiply by the sizeof(int) to get the true number of bytes that need to be allocateted
 
     // realloc takes a pointer to existting allocated memory, and resizes it
 }
